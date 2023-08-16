3e47236d7488c1fd68dcc142bc81e8ea5901c8ee
HDRP <hdrp0720@gmail.com>
2023-08-16 14:39:30 +0900

    add logic of camera & player movement

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+  [SerializeField] private float moveSpeed = 7f;
+  [SerializeField] private float jumpPower = 10f;
+
+  private CharacterController cc;
+  private float gravity = -20f;
+  private float yVelocity = 0;
+  private bool bIsJumping = false;
+
+  private void Start() 
+  {
+    cc = GetComponent<CharacterController>();
+  }
+  private void Update() 
+  {
+    if(cc.collisionFlags != CollisionFlags.Below)    
+      yVelocity += gravity * Time.deltaTime;
+
+    float h = Input.GetAxis("Horizontal");
+    float v = Input.GetAxis("Vertical");
+
+    Vector3 dir = new Vector3(h, 0, v);
+    dir = dir.normalized;
+
+    dir = Camera.main.transform.TransformDirection(dir);
+    if(bIsJumping && cc.collisionFlags == CollisionFlags.Below)
+    {
+      bIsJumping = false;
+      yVelocity = 0;
+    }
+
+    if(Input.GetButtonDown("Jump") && !bIsJumping)
+    {
+      yVelocity = jumpPower;
+      bIsJumping = true;      
+    }    
+    
+    dir.y = yVelocity; 
+
+    // transform.position += dir * moveSpeed * Time.deltaTime;
+    cc.Move(dir * moveSpeed * Time.deltaTime);
+  }
+}
